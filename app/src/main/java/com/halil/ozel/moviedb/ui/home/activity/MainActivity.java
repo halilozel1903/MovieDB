a1a119a94b5a03d43792272109b6b835a9b3aa7f
halilozel1903 <halilozel1903@gmail.com>
2022-05-08 21:32:30 +0300

    Android 12 & Gradle 7 settings were added.

--- app/src/main/java/com/halil/ozel/moviedb/ui/home/activity/MainActivity.java
+++ app/src/main/java/com/halil/ozel/moviedb/ui/home/activity/MainActivity.java
@@ -1,18 +1,17 @@
 package com.halil.ozel.moviedb.ui.home.activity;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
 import com.halil.ozel.moviedb.App;
-import com.halil.ozel.moviedb.ui.home.adapters.MovieAdapter;
 import com.halil.ozel.moviedb.R;
 import com.halil.ozel.moviedb.data.Api.TMDbAPI;
 import com.halil.ozel.moviedb.data.models.Results;
-
+import com.halil.ozel.moviedb.ui.home.adapters.MovieAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,23 +31,20 @@ public class MainActivity extends AppCompatActivity {
     TMDbAPI tmDbAPI;
 
     public RecyclerView rvPopularMovie;
-    public  RecyclerView.Adapter popularMovieAdapter;
+    public RecyclerView.Adapter popularMovieAdapter;
     public RecyclerView.LayoutManager popularMovieLayoutManager;
-    public  List<Results> popularMovieDataList;
+    public List<Results> popularMovieDataList;
 
     public RecyclerView rvNowPlaying;
-    public  RecyclerView.Adapter nowPlayingMovieAdapter;
+    public RecyclerView.Adapter nowPlayingMovieAdapter;
     public RecyclerView.LayoutManager nowPlayingLayoutManager;
-    public  List<Results> nowPlayingDataList;
+    public List<Results> nowPlayingDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         App.instance().appComponent().inject(this);
         setContentView(R.layout.activity_main);
-
-//        Objects.requireNonNull(getSupportActionBar()).setTitle("Movies");
-
 
         popularMovieDataList = new ArrayList<>();
         popularMovieAdapter = new MovieAdapter(popularMovieDataList, this);
@@ -72,40 +68,35 @@ public class MainActivity extends AppCompatActivity {
         rvNowPlaying.setAdapter(nowPlayingMovieAdapter);
 
 
-
         getPopularMovies();
         getNowPlaying();
     }
 
 
-    public void getNowPlaying(){
+    @SuppressLint("NotifyDataSetChanged")
+    public void getNowPlaying() {
 
         tmDbAPI.getNowPlaying(TMDb_API_KEY, 1)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(response -> {
 
-                    for (Results results : response.getResults()){
-
-                        popularMovieDataList.add(results);
-                    }
+                    popularMovieDataList.addAll(response.getResults());
 
                     popularMovieAdapter.notifyDataSetChanged();
 
                 }, e -> Timber.e(e, "Error fetching now popular movies: %s", e.getMessage()));
     }
 
-    public void getPopularMovies(){
+    @SuppressLint("NotifyDataSetChanged")
+    public void getPopularMovies() {
 
         tmDbAPI.getPopularMovie(TMDb_API_KEY, 1)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(response -> {
 
-                    for (Results results : response.getResults()){
-
-                        nowPlayingDataList.add(results);
-                    }
+                    nowPlayingDataList.addAll(response.getResults());
 
                     nowPlayingMovieAdapter.notifyDataSetChanged();
 
