0f297896f88ab834e6aef900dc267d25bb90a353
Halil Özel <halilozel1903@gmail.com>
2025-06-21 17:56:44 +0300

    Add cast detail screen and UI improvements

---
+++ app/src/main/java/com/halil/ozel/moviedb/data/models/PersonDetail.java
@@ -0,0 +1,52 @@
+package com.halil.ozel.moviedb.data.models;
+
+import java.io.Serializable;
+
+public class PersonDetail implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String biography;
+    private String birthday;
+    private String profile_path;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getProfile_path() {
+        return profile_path;
+    }
+
+    public void setProfile_path(String profile_path) {
+        this.profile_path = profile_path;
+    }
+}
