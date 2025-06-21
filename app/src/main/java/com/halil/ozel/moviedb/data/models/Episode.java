c1ec9b59a86b0108e17e22f6fd1e25792b6992a6
Halil Özel <halilozel1903@gmail.com>
2025-06-21 21:09:06 +0300

    Add season and episode detail dialogs

--- app/src/main/java/com/halil/ozel/moviedb/data/models/Episode.java
+++ app/src/main/java/com/halil/ozel/moviedb/data/models/Episode.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 public class Episode implements Serializable {
     private Integer episode_number;
     private String name;
+    private String overview;
+    private String still_path;
 
     public Integer getEpisode_number() {
         return episode_number;
@@ -20,5 +22,21 @@ public class Episode implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getStill_path() {
+        return still_path;
+    }
+
+    public void setStill_path(String still_path) {
+        this.still_path = still_path;
     }
 }
