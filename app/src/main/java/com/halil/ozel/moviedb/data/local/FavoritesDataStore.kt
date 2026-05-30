de21c446cd8036ea79c5456ba6c27148c46818c1
Halil Ozel <halilozel1903@gmail.com>
2026-05-30 13:51:59 +0300

    Refactor: remove legacy Java components and layouts, initialize Hilt Application class
    - Remove legacy Java fragments and SharedPreferences manager
    - Delete unused XML layout files
    - Add custom `Application` class with Hilt and Timber support
    - Initialize `FavoritesDataStore` configuration

---
+++ app/src/main/java/com/halil/ozel/moviedb/data/local/FavoritesDataStore.kt
@@ -0,0 +1,4 @@
+package com.halil.ozel.moviedb.data.local
+
+// DataStore is configured in di/DataStoreModule.kt
+// FavoritesRepository implementation is in data/repository/FavoritesRepositoryImpl.kt
