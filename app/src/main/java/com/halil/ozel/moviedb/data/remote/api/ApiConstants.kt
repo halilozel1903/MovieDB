f0c5dafed1b1b56e4c0bf4bfedd8a5cb2e441d28
Halil Ozel <halilozel1903@gmail.com>
2026-05-30 13:50:58 +0300

    Refactor project structure and remove legacy activities
    - Remove `AllMoviesActivity`, `AllTvActivity`, and various detail activities
    - Delete associated XML layout files for removed activities
    - Update `AndroidManifest.xml` to use a single activity entry point and new theme
    - Centralize API URLs in new `ApiConstants.kt`
    - Remove legacy `App` class and associated Dagger injections

---
+++ app/src/main/java/com/halil/ozel/moviedb/data/remote/api/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.halil.ozel.moviedb.data.remote.api
+
+object ApiConstants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL_500 = "https://image.tmdb.org/t/p/w500"
+    const val IMAGE_BASE_URL_1280 = "https://image.tmdb.org/t/p/w1280"
+    const val PROFILE_BASE_URL = "https://image.tmdb.org/t/p/w185"
+}
